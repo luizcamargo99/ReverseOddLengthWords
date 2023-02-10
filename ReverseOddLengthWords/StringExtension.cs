5e8a356dce5021cb6293bf5fa9b74c44e5f16bc7
Luiz Camargo <luiz.zito@hotmail.com>
2023-02-10 13:21:07 +0000

    update

--- ReverseOddLengthWords/StringExtension.cs
+++ ReverseOddLengthWords/StringExtension.cs
@@ -2,13 +2,13 @@
 
 public static class StringExtension
 {
+    private static string _space { get { return " "; } }
+
     public static string ReverseOdd(this string str)
     {
         var newStr = string.Empty;
         var strArray = str.Split();
-        for (int i = 0; i < strArray.Length; i++) newStr += strArray[i].Length % 2 == 0 ? strArray[i] + Space() : string.Join(string.Empty, strArray[i].Reverse()) + Space();
+        for (int i = 0; i < strArray.Length; i++) newStr += strArray[i].Length % 2 == 0 ? strArray[i] + _space : string.Join(string.Empty, strArray[i].Reverse()) + _space;
         return newStr.TrimEnd();
     }
-
-    private static string Space() => " ";
 }
